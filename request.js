e49e420b48615776055bb9d204241f027105c289
Louis <louisprotano@hotmail.com>
2024-04-12 12:41:27 +0100

    stuff

--- request.js
+++ request.js
@@ -1,5 +1,20 @@
+// fetch("https://dev.io-academy.uk/resources/bond.php").then((response) => {
+//     return response.json()
+// }).then((data) => {
+//     console.log(data)
+//     d = data.data
+//     document.querySelector('.output').innerHTML = d.forEach(item => {
+//         return `<li> ${item.name}</li>`
+//     })
+    
+// })
+
 fetch("https://dev.io-academy.uk/resources/bond.php").then((response) => {
     return response.json()
 }).then((data) => {
     console.log(data)
+    d = data.data
+    document.querySelector('.output').innerHTML = for(i=0; i < d.length; i++){
+        
+    }
 })
